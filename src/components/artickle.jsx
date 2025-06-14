6e2d917fddd4f8e8104530c3b3442522d93cc310
KenzArz <kenzart05@gmail.com>
2025-06-14 12:35:47 +0700

    ]

--- src/components/artickle.jsx
+++ src/components/artickle.jsx
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import "../css/articles.css";
 
 import { BlogContent } from "../js/blog-content";
 
 export default function () {
+	const navigate = useNavigate();
 	return (
 		<>
 			<div className="more-article">
 				{BlogContent.map((article, index) => (
-					<div className="article-card" key={index}>
+					<div
+						className="article-card"
+						key={index}
+						onClick={() => navigate(`/blog/${index + 1}`)}
+					>
 						<img src={article.image} />
 						<p>{article.title}</p>
 					</div>
